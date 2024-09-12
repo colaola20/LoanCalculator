85da3d70aafb317679c5942a969bd4a3bf9d3d96
colaola20 <soryo@farmingdale.edu>
2024-09-11 21:29:25 -0400

    Setting the Grid Pane to the CENTER

--- src/main/java/org/example/loancalculator/HelloApplication.java
+++ src/main/java/org/example/loancalculator/HelloApplication.java
@@ -2,6 +2,7 @@ package org.example.loancalculator;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.layout.GridPane;
@@ -18,11 +19,12 @@ public class HelloApplication extends Application {
         Label line2 = new Label("Number of Years:");
         Label line3 = new Label("Loan Amount");
         Label line4 = new Label("Monthly Payment:");
-        Label line5 = new Label("Total  Payment");
+        Label line5 = new Label("Total  Payment:");
 
         GridPane gridPane = new GridPane();
         gridPane.setHgap(10);
         gridPane.setVgap(10);
+        gridPane.setAlignment(Pos.CENTER);
         gridPane.add(line1, 0, 0);
         gridPane.add(line2, 0, 1);
         gridPane.add(line3, 0, 2);
