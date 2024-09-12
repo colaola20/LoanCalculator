5074fc2e83019117af5b630c66fd479e3e26a519
colaola20 <soryo@farmingdale.edu>
2024-09-11 21:39:19 -0400

    Creating the text fields and adding them to the Grid Pane

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module org.example.loancalculator {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.desktop;
 
 
     opens org.example.loancalculator to javafx.fxml;
--- src/main/java/org/example/loancalculator/HelloApplication.java
+++ src/main/java/org/example/loancalculator/HelloApplication.java
@@ -5,9 +5,11 @@ import javafx.fxml.FXMLLoader;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 
+import javax.swing.*;
 import java.io.IOException;
 
 public class HelloApplication extends Application {
@@ -15,24 +17,40 @@ public class HelloApplication extends Application {
     public void start(Stage stage) throws IOException {
         stage.setTitle("LoanCalculator");
 
-        Label line1 = new Label("Annual Interest Rate:");
-        Label line2 = new Label("Number of Years:");
-        Label line3 = new Label("Loan Amount");
-        Label line4 = new Label("Monthly Payment:");
-        Label line5 = new Label("Total  Payment:");
+        Label textLine1 = new Label("Annual Interest Rate:");
+        Label textLine2 = new Label("Number of Years:");
+        Label textLine3 = new Label("Loan Amount");
+        Label textLine4 = new Label("Monthly Payment:");
+        Label textLine5 = new Label("Total  Payment:");
+
+        TextField textField1 = new TextField();
+        TextField textField2 = new TextField();
+        TextField textField3 = new TextField();
+        TextField textField4 = new TextField();
+        TextField textField5 = new TextField();
+        textField1.setPrefWidth(200);
+        textField2.setPrefWidth(200);
+        textField3.setPrefWidth(200);
+        textField4.setPrefWidth(200);
+        textField5.setPrefWidth(200);
 
         GridPane gridPane = new GridPane();
-        gridPane.setHgap(10);
-        gridPane.setVgap(10);
+        gridPane.setHgap(5);
+        gridPane.setVgap(5);
         gridPane.setAlignment(Pos.CENTER);
-        gridPane.add(line1, 0, 0);
-        gridPane.add(line2, 0, 1);
-        gridPane.add(line3, 0, 2);
-        gridPane.add(line4, 0, 3);
-        gridPane.add(line5, 0, 4);
+        gridPane.add(textLine1, 0, 0);
+        gridPane.add(textLine2, 0, 1);
+        gridPane.add(textLine3, 0, 2);
+        gridPane.add(textLine4, 0, 3);
+        gridPane.add(textLine5, 0, 4);
+        gridPane.add(textField1, 1, 0);
+        gridPane.add(textField2, 1, 1);
+        gridPane.add(textField3, 1, 2);
+        gridPane.add(textField4, 1, 3);
+        gridPane.add(textField5, 1, 4);
 
 
-        Scene scene = new Scene(gridPane, 320, 240);
+        Scene scene = new Scene(gridPane, 350, 240);
         stage.setTitle("LoanCalculator");
         stage.setScene(scene);
         stage.show();
